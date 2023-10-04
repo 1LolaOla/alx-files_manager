e8d0c2335f7f6112901bb785e47226b03baa2214
1LolaOla <funmiolay1@gmail.com>
2023-10-04 09:07:41 -0700

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-import express from 'express';
-import router from './routes/index';
-
-const port = parseInt(process.env.PORT, 10) || 5000;
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
+const port = parseInt(process.env.PORT, 10) || 5000;
 
-app.use(express.json());
-app.use('/', router);
+app.use(express.json({ limit: '2mb' }));
+app.use(router);
 
-app.listen(port, () => {
+app.listen(port, 'localhost', () => {
   console.log(`server running on port ${port}`);
 });
-
-export default app;
