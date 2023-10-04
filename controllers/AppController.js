187d645b2d7f6d499c6005b668916aae27646c9c
1LolaOla <funmiolay1@gmail.com>
2023-10-04 09:17:48 -0700

    AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,19 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
-class AppController {
-  static getStatus (request, response) {
-    const status = {
-      redis: RedisClient.isAlive(),
-      db: DBClient.isAlive()
-    };
-    return response.status(200).send(status);
-  }
-
-  static async getStats (request, response) {
-    const stats = {
-      users: await DBClient.nbUsers(),
-      files: await DBClient.nbFiles()
-    };
-    return response.status(200).send(stats);
-  }
+function getStatus(req, res) {
+  const redisAlive = redisClient.isAlive();
+  const dbAlive = dbClient.isAlive();
+  res.send({ redis: redisAlive, db: dbAlive });
 }
 
-module.exports = AppController;
+async function getStats(req, res) {
+  const numUsers = await dbClient.nbUsers();
+  const numFiles = await dbClient.nbFiles();
+  res.send({ users: numUsers, files: numFiles });
+}
+
+module.exports = {
+  getStats,
+  getStatus,
+};
